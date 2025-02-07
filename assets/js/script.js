4a0e4b0b147a2c7eed2727b60ac65d420e118915
KaustubhD15 <113599528+KaustubhD15@users.noreply.github.com>
2025-02-07 00:54:03 -0600

    added certifications

--- assets/js/script.js
+++ assets/js/script.js
@@ -238,3 +238,7 @@ srtop.reveal('.experience .timeline .container', { interval: 400 });
 /* SCROLL CONTACT */
 srtop.reveal('.contact .container', { delay: 400 });
 srtop.reveal('.contact .container .form-group', { delay: 400 });
+
+function viewCertifications() {
+    window.location.href = "certifications.html"; // Change this to the actual page URL
+}
